f8863ac06cb3630ef2c98bc32cd86e495f41771b
Jeremy zhao <zhaoyimeng@foxmail.com>
2020-02-11 22:48:15 +0800

    优化

--- interviewQuestions/comprehension.py
+++ interviewQuestions/comprehension.py
@@ -9,11 +9,11 @@ list2 = list(map(lambda x : x**2, list1))
 print(list2)
 
 # 列表推导式
-list3 = [i**2  for i in list1]
+list3 = [i**2 for i in list1]
 print(list3)
 
 # 字典推导式
-dict1 = {'a':1, 'b':2, 'c':3}
-dict2 = {k:v for k,v in dict1.items()}
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {k: v for k, v in dict1.items()}
 print(dict2)
 
